1d91658fd199258349242b39271b422df8848258
acastromejia <arielcastromejia@gmail.com>
2023-01-30 15:59:20 -0600

    Inyeccion de dependencias en otra dependencia, platzi 11 y 12.

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
@@ -0,0 +1,16 @@
+package com.fundamentosplatzi.springboot.fundamentos.bean;
+
+public class MyBeanWithDependencyImplement implements MyBeanWithDependency{
+    MyOperation myOperation;
+
+    public MyBeanWithDependencyImplement(MyOperation myOperation) {
+        this.myOperation = myOperation;
+    }
+
+    @Override
+    public void printWithDependency() {
+        int numero=1;
+        System.out.println(myOperation.sum(numero));
+        System.out.println("Print desde la clase MybeanWithDependencyImplement: Bean con Independencia");
+    }
+}
